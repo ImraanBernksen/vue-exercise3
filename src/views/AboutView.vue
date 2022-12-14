39a66076cef0fab1a832ad3acb37a362025f999c
ImraanBernksen <imraanbernksen@gmail.com>
2022-12-14 12:05:48 +0200

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <NavBar/>
+  <h1>About</h1>
+  <Footer/>
+</template>
+<script>
+import NavBar from '../components/NavBar.vue';
+import Footer from '../components/Footer.vue'
+export default {
+  components: {
+    NavBar,
+    Footer
+  }
+}
+</script>
+<style scoped>
+  
+</style>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,21 @@
+<template>
+  <NavBar/>
+  <h1>Products</h1>
+  <Products/>
+  <Footer/>
+</template>
+<script>
+import NavBar from '../components/NavBar.vue';
+import Products from '../components/Products.vue';
+import Footer from '../components/Footer.vue'
+export default {
+  components: {
+    NavBar,
+    Products,
+    Footer
+  }
+}
+  </script>
+  <style scoped>
+    
+  </style>
